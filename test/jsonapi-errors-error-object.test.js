fd6b65a3793bfc7dc8994de5b9d022132a86d254
poddaan <Ananya_Poddar@manulife.com>
2021-12-16 14:19:14 -0500

    Add error obj type tests

--- test/jsonapi-errors-error-object.test.js
+++ test/jsonapi-errors-error-object.test.js
@@ -1,9 +1,21 @@
 'use strict';
 
+const {join} = require('path');
 const {expect} = require('chai');
+const {Spectral, Document, Parsers} = require('@stoplight/spectral');
 const {JSONPath} = require('jsonpath-plus');
 
+const RULESET_FILE = join(__dirname, '../rules/jsonapi-errors-error-object.yaml');
+
 describe('jsonapi-errors-error-object ruleset:', function () {
+
+  let spectral;
+
+  beforeEach(function () {
+
+    spectral = new Spectral();
+
+  });
 
   describe('jsonapi-errors-error-object', function () {
 
@@ -43,6 +55,86 @@ describe('jsonapi-errors-error-object ruleset:', function () {
       expect(results.length).to.equal(1, 'Wrong number of results.');
       expect(results).to.deep.equal(expectedPaths, 'Wrong paths');
       done();
+
+    });
+
+    it('the rule should return "error-object-type" error if using JSON:API and error-object is not an array', function (done) {
+
+      const badDocument = new Document(`
+        openapi: 3.0.2
+        paths:
+          /stuff:
+            get:
+              responses:
+                '200':
+                  content:
+                    application/vnd.api+json:
+                      schema:
+                        type: object
+                        properties:
+                          errors:
+                            type: string
+        `, Parsers.Yaml);
+
+      spectral.loadRuleset(RULESET_FILE)
+        //remove rule(s) we aren't testing
+        .then(() => {
+
+          delete spectral.rules['error-object-properties'];
+
+        })
+        .then(() => {
+
+          return spectral.run(badDocument);
+
+        })
+        .then((results) => {
+
+          expect(results.length).to.equal(1, 'Error count should be 1');
+          expect(results[0].code).to.equal('error-object-type', 'Incorrect error');
+          expect(results[0].path.join('/')).to.equal('paths//stuff/get/responses/200/content/application/vnd.api+json/schema/properties/errors/type', 'Wrong path');
+          done();
+
+        });
+
+    });
+
+    it('the rule should return no type errors', function (done) {
+
+      const cleanDocument = new Document(`
+        openapi: 3.0.2
+        paths:
+          /stuff:
+            get:
+              responses:
+                '200':
+                  content:
+                    application/vnd.api+json:
+                      schema:
+                        type: object
+                        properties:
+                          errors:
+                            type: array
+        `, Parsers.Yaml);
+
+      spectral.loadRuleset(RULESET_FILE)
+        //remove rule(s) we aren't testing
+        .then(() => {
+
+          delete spectral.rules['error-object-properties'];
+
+        })
+        .then(() => {
+
+          return spectral.run(cleanDocument);
+
+        })
+        .then((results) => {
+
+          expect(results.length).to.equal(0, 'Error(s) found');
+          done();
+
+        });
 
     });
 
