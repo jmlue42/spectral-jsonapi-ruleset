f1c680dcca894bf584393b97eea00a414bdd0579
Jeff Marquez <jmarquez@manulife.com>
2023-11-16 14:57:41 -0700

    - Updating linting to lint .js files. - fixing funciton name (incorrectly used capital letter 'Match') - updating files to match linting rules

--- rules/jsonapi-member-names.js
+++ rules/jsonapi-member-names.js
@@ -3,7 +3,7 @@
 // All rules in this file MUST have corresponding tests
 
 
-import { enumeration, pattern, casing} from '@stoplight/spectral-functions';
+import { pattern } from '@stoplight/spectral-functions';
 import { DiagnosticSeverity } from '@stoplight/types';
 
 export default {
@@ -12,21 +12,20 @@ export default {
     'member-names': {
       description: 'Implementation specific query parameters MUST adhere to the same constraints as member names along with additional requirements.',
       message: '{{path}} - {{description}}',
-      severity: DiagnosticSeverity.Error, // error
-      // query may be more like a POST https://dev.wix.com/api/rest/members/members/query-members
-      // given: "$.paths[*][get]",   // "$..properties[?(@property === 'query')]",
+      severity: DiagnosticSeverity.Error,
       given: [
-        "$.paths[*][get].parameters.name",
-        "$.paths[*][get].parameters[*].name"
+        '$.paths[*][get].parameters.name',
+        '$.paths[*][get].parameters[*].name'
       ],
       then: {
-        //field: "@key", //The value can also be @key to apply the rule to the keys of an object.
-        function: pattern, //I want to apply ALL Matches listed below to ALL Members
+        // field: "@key", //The value can also be @key to apply the rule to the keys of an object.
+        function: pattern, // I want to apply ALL Matches listed below to ALL Members
         functionOptions: {
-          // 4 regex may have to be expanded to 4 rules
-          Match: "^[a-zA-Z0-9]",
-          Match: "[a-zA-Z0-9]$",
-          Match: "[a-zA-Z0-9-_]"
+          match: [
+            '^[a-zA-Z0-9]',
+            '[a-zA-Z0-9]$',
+            '[a-zA-Z0-9-_]'
+          ]
         }
       }
     }
