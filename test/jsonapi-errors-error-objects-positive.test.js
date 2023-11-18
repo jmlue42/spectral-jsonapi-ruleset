500bc600bb533cf1b79b834fae9d602aa26fa949
ezenity <anthonymmacallister@gmail.com>
2023-11-18 01:43:30 -0500

    Refactor: Following other test case formats
    - will provide will cases for each rule, same as other test cases:
    
    * Validate JSONPath
    * Positive Scenario
    * Negative Scenario
    
    NOTE: Currently the 'Validate JSONPath' will need additional research since `JSONPath` does not inherently resolve '$ref' references found in JSON documents or OpenAPI specifications.
    
    POTENTIAL SOLUTION:
    * Preprcess the Document
      - Before applying JSONPath queries, preprocess the JSON document to resolve and replace all `$ref` references with the actual content they point to. This `dereferencing1 process can be accomplised using a library called 'json-schema-ref-parser'. This would allow us to use `jsonpath-plus`.
    * Custom Handling
      - Can write custom logic to handle `$ref` references. This does require more work and would need to know the specific JSON document structure.

--- test/jsonapi-errors-error-objects-positive.test.js
+++ test/jsonapi-errors-error-objects-positive.test.js
@@ -1,25 +1,20 @@
 /* eslint-env mocha */
-/* eslint-disable no-console */
-/* eslint-disable no-invalid-this */
 import { expect } from 'chai';
+// import { JSONPath } from 'jsonpath-plus';
 
 // Rules to test
 import ruleset from '../rules/jsonapi-errors-error-object.js';
 
 // Helper Functions
-import setupSpectral from './utils/setupSpectral.js';
 import { handleSpectralResults } from './utils/handleSpectralResults.js';
-import { getAllRulesets } from './utils/getAllRulesets.js';
-import { getRulesFromTestTitle } from './utils/getRulesFromTestTitle.js';
-import { enableSpecificRuleset } from './utils/enableSpecificRuleset.js';
-import { displayRulesets } from './utils/displayRulesets.js';
-import { formattedErrorMessage } from './utils/formattedErrorMessage.js';
 import { processErrors } from './utils/processErrors.js';
-import { debugDebug, debugLog } from './utils/debugUtils.js';
+import { debugLog } from './utils/debugUtils.js';
+import { setupSpectralBeforeEach } from './utils/setupSpectralBeforeEach.js';
 
-// Valid OpenAPI Documents
+// OpenAPI Documents
 // import validArrayStructureDocument from './docs/errors/errorObjects/validArrayStructureDocument.js';
 import validApiDocument from './docs/errors/errorObjects/validApiDocument.js';
+import invalidApiDocument from './docs/errors/errorObjects/invalidApiDocument.js';
 
 /**
  * @fileoverview This test suite validates the behavior of the JSON: API fetching data ruleset
@@ -38,84 +33,129 @@ import validApiDocument from './docs/errors/errorObjects/validApiDocument.js';
  * 
  * Each test focuses on one of the following rules, aiming to NOT trigger any errors:
  */
-describe('jsonapi-errors-error-objects ruleset passing scenarios:', function errorsErrorObjectsPassingSuite() {
+describe('jsonapi-errors-error-objects ruleset:', function errorsErrorObjectsSuite() {
 
-  let spectral;
-  beforeEach(function errorsErrorObjectsPassingBeforeEach() {
+  describe('errors-error-objects-array-structure:', function errorsErrorObjectsArrayStructure() {
 
-    try {
+    const testingRuleName = 'errors-error-objects-array-structure';
 
-      spectral = setupSpectral(ruleset);
+    beforeEach(setupSpectralBeforeEach(ruleset, [testingRuleName]));
 
-      this.currentTestTitle = this.currentTest.title;
+    // it.only('the json path expression should find the correct paths from the given document', async function errorsErrorObjectsArrayStructurePath() {
 
-      const rulesFromTitle = getRulesFromTestTitle(this.currentTestTitle, getAllRulesets(ruleset));
+    //   try {
+    
+    //     const relevantResults = await handleSpectralResults(this.spectral, validApiDocument, testingRuleName);
+          
+    //     // debugDebug(`Testing Rule Description: \x1b[36m${this.spectral.ruleset.rules[testingRuleName].description}\x1b[0m\n`);
+    //     const propsToInclude = ['path'];
+    //     const jsData = JSON.stringify(relevantResults, propsToInclude, 2);
+    //     debugDebug(`Testing Rule Path: \x1b[36m${jsData}\x1b[0m\n`);
 
-      if (rulesFromTitle && rulesFromTitle.length > 0) {
+    //     debugDebug(ruleset.rules[testingRuleName].given);
+    
+    //     const jsonPathExpression = ruleset.rules[testingRuleName].given;
+    //     const expectedPaths = [
+    //       validApiDocument.paths['/users/{userId}'].get.responses[400]
+    //       // validApiDocument.paths['/users/{userId}'].get.responses[404].content['application/vnd.api+json.api+json'].schema.properties,
+    //       // validApiDocument.paths['/users/{userId}'].get.responses[500].content['application/vnd.api+json.api+json'].schema.properties,
+    //     ]
 
-        enableSpecificRuleset(spectral, rulesFromTitle);
+    //     const results = JSONPath("$..[?(@property >= '400' && @property <= '599')]", validApiDocument);
+
+    //     debugLog(`  Confirmed Errors:`);
+    //     debugLog(`\x1b[33m    - ${results.length}\x1b[0m\n`);
+
+    //     expect(results.length).to.equal(3, `\x1b[31mExpected results to equal \x1b[33m3\x1b[31m, instead \x1b[33m${results.length}\x1b[31m was found.\x1b[0m\n`);
+    //     expect(results).to.deep.equal(expectedPaths, `\x1b[31mThe wrong JSONPaths were provided.\x1b[0m\n`);
+
+    //     // const errorMessage = `\x1b[31mError count should be 0.\n\x1b[31m Failing Ruleset Details: \x1b[31m`;
+    //     // const propsToInclude = ['message', 'path'];
+    //     // const jsData = JSON.stringify(relevantResults, propsToInclude, 2);
+    //     // expect(relevantResults.length).to.equal(0, `${errorMessage} \x1b[33m${jsData.replace(/", /gu, `",\n`)}\x1b[0m`);
+        
+    //   } catch (error) {
+    
+    //     processErrors(error);
+        
+    //   }
       
+    // });
+
+    it('the rule should return "errors-error-objects-array-structure" errors if errors property type is NOT an array', async function errorsErrorObjectsFailureArrayStructure() {
+
+      try {
+    
+        const relevantResults = await handleSpectralResults(this.spectral, invalidApiDocument, testingRuleName);
+    
+        debugLog(`  Confirmed Errors:`);
+        debugLog(`\x1b[33m    - ${relevantResults.length}\x1b[0m\n`);
+    
+        const errorMessage = `\x1b[31mError count should be \x1b[33m3.\n\x1b[31m Failing Ruleset Details: \x1b[31m`;
+        const propsToInclude = ['message', 'path'];
+        const jsData = JSON.stringify(relevantResults, propsToInclude, 2);
+        expect(relevantResults.length).to.equal(3, `${errorMessage} \x1b[33m${jsData.replace(/", /gu, `",\n`)}\x1b[0m`);
+        
+      } catch (error) {
+    
+        processErrors(error);
+        
       }
+      
+    });
 
-      displayRulesets(spectral);
-    
-    } catch (error) {
+    it('the rule should pass with NO errors', async function errorsErrorObjectsPassingArrayStructure() {
 
-      processErrors(error);
+      try {
     
-    }
-  
+        const relevantResults = await handleSpectralResults(this.spectral, validApiDocument, testingRuleName);
+    
+        debugLog(`  Confirmed Errors:`);
+        debugLog(`\x1b[33m    - ${relevantResults.length}\x1b[0m\n`);
+    
+        const errorMessage = `\x1b[31mError count should be 0.\n\x1b[31m Failing Ruleset Details: \x1b[31m`;
+        const propsToInclude = ['message', 'path'];
+        const jsData = JSON.stringify(relevantResults, propsToInclude, 2);
+        expect(relevantResults.length).to.equal(0, `${errorMessage} \x1b[33m${jsData.replace(/", /gu, `",\n`)}\x1b[0m`);
+        
+      } catch (error) {
+    
+        processErrors(error);
+        
+      }
+      
+    });
+
   });
 
-  it('should pass with no errors for |errors-error-objects-array-structure|', async function errorsErrorObjectsPassingArrayStructure() {
+  // describe('errors-error-objects-object-structure', function errorsErrorObjectsObjectStructure() {
 
-    try {
+  //   beforeEach(setupSpectralBeforeEach(ruleset, ['errors-error-objects-object-structure']));
 
-      // const relevantResults = await handleSpectralResults(spectral, validArrayStructureDocument, 'errors-error-objects-array-structure');
-      const relevantResults = await handleSpectralResults(spectral, validApiDocument, 'errors-error-objects-array-structure');
+  //     it('should pass with no errors for ....', async function errorsErrorObjectsPassingObjectStructure() {
 
-      debugDebug(`\x1b[35mSpectral JSONPath:\x1b[36m ${JSON.stringify(spectral.ruleset.rules['errors-error-objects-array-structure'].given)}\x1b[0m\n`);
+  //       try {
 
-      debugLog(`  Confirmed Errors:`);
-      debugLog(`\x1b[33m    - ${relevantResults.length}\x1b[0m\n`);
+  //         const relevantResults = await handleSpectralResults(this.spectral, validApiDocument, 'errors-error-objects-object-structure');
 
-      const errorMessage = `
-              \x1b[31mError count should be 0 for Array Structure within OpenAPI structure.\n
-              \x1b[31mFailing Ruleset Details: \x1b[0m
-          `;
-      const jsData = JSON.stringify(relevantResults, null, 2);
-      expect(relevantResults.length).to.equal(0, errorMessage + jsData.replace(/", /gu, `",\n`));
-    
-    } catch (error) {
+  //         debugLog(`  Confirmed Errors:`);
+  //         debugLog(`\x1b[33m    - ${relevantResults.length}\x1b[0m\n`);
 
-      throw new Error(formattedErrorMessage(error));
-    
-    }
-  
-  });
+  //         const errorMessage = `
+  //                 \x1b[31mError count should be 0 for Object Structure within OpenAPI structure.\n
+  //                 \x1b[31mFailing Ruleset Details: \x1b[0m
+  //             `;
+  //         const jsData = JSON.stringify(relevantResults, null, 2);
+  //         expect(relevantResults.length).to.equal(0, errorMessage + jsData.replace(/", /gu, `",\n`));
+        
+  //       } catch (error) {
 
-  it('should pass with no errors for |errors-error-objects-object-structure|', async function errorsErrorObjectsPassingObjectStructure() {
+  //         throw new Error(formattedErrorMessage(error));
+        
+  //       }
 
-    try {
+  //     });
 
-      const relevantResults = await handleSpectralResults(spectral, validApiDocument, 'errors-error-objects-object-structure');
-
-      debugLog(`  Confirmed Errors:`);
-      debugLog(`\x1b[33m    - ${relevantResults.length}\x1b[0m\n`);
-
-      const errorMessage = `
-              \x1b[31mError count should be 0 for Object Structure within OpenAPI structure.\n
-              \x1b[31mFailing Ruleset Details: \x1b[0m
-          `;
-      const jsData = JSON.stringify(relevantResults, null, 2);
-      expect(relevantResults.length).to.equal(0, errorMessage + jsData.replace(/", /gu, `",\n`));
-    
-    } catch (error) {
-
-      throw new Error(formattedErrorMessage(error));
-    
-    }
-
-  });
+  // });
       
 });
