64357ccf37892c1c68f4fb2be671ef68d6d56574
ezenity <anthonymmacallister@gmail.com>
2023-12-07 22:17:07 -0500

    Refactor: Spectral's JSON Formatter Outputs
    - When running your test cases with the Spectral instance, Spectral will provide an analysis to ensure to can run rule and pass the validations along with reading rulename and/or provided document. This addition will provide a better understanding as to why the test case of rule is not running properly.

--- test/utils/handleSpectralResults.js
+++ test/utils/handleSpectralResults.js
@@ -35,8 +35,25 @@ export async function handleSpectralResults(spectral, document, resultCode) {
     // Log each result, including targetVal
     results.forEach((result) => {
 
-      debugDebug(`Result look check, result code: \x1b[32m${result.code}\x1b[35m, target code: \x1b[32m${resultCode}\x1b[35m.\n`);
+      debugDebug(`Handle Spectral Results Utils - Spectral's Run Output\n`);
+      
+      // Spectral provides an output of the results for reasons why it has potentially failed, below are the details to possibly fix them.
+      debugDebug(`Result code: \x1b[32m${result.code}\x1b[0m`);
+      // Since we are mapping the results path, we need to confirm that is an a provided path to avoid a `TypeError: undefined` message.
+      if (result.path) {
 
+        debugDebug(`Result path: \x1b[33m[ \x1b[32m${result.path.map((item) => `\x1b[33m"\x1b[32m${item}\x1b[33m"`).join(', ')} \x1b[33m]\x1b[0m`);
+      
+      }
+      debugDebug(`Result message: \x1b[32m${result.message}\x1b[0m`);
+      // debugDebug(`Result severity: \x1b[32m${result.severity}\x1b[0m`);
+      // debugDebug(`Result range: \x1b[32m${JSON.stringify(result.range, null, 2)}\x1b[0m`);
+      // debugDebug(`Result source: \x1b[32m${result.source}\x1b[0m\n`);
+
+
+      // If the rule name is found in both `result.code` & resultCode`, this statement will process
+      // If the rule name does NOT match, this statement will not run and will need to review the
+      // `Result Code` output for potential issues with Spectral Run has failed.
       if (result.code === resultCode) {
 
         debugDebug(`\x1b[32mResult for '${resultCode}':\x1b[36m ${JSON.stringify(result, ['message', 'path'], 2)} \x1b[0m\n`);
